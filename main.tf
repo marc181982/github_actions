fc9cb19cf5c0b249657a62d1a60ae0043095d73a
randolf-asml <65965383+randolf-asml@users.noreply.github.com>
2020-06-25 22:49:06 +0200

    Updated Format

--- main.tf
+++ main.tf
@@ -1,15 +1,14 @@
-#Terraform Cloud Configuration
 terraform {
     backend "remote" {
-    organization = "terratestomgeving"
-        workspaces {
-        name = "azure-landingzone-team-01"
+        organization = "terratestomgeving"
+            workspaces {
+                name = "azure-landingzone-team-01"
         }
     }
 }
 
 resource "null_resource" "example" {
     triggers = {
-    value = "A example resource that does nothing!"
+        value = "A example resource that does nothing!"
     }
 }
