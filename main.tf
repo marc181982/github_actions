db1cc67e619ac02325933cb26a73eaa0415cbd80
randolf-asml <65965383+randolf-asml@users.noreply.github.com>
2020-06-25 22:41:42 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+    backend "remote" {
+    organization = "terratestomgeving"
+        workspaces {
+        name = "azure-landingzone-team-01"
+        }
+    }
+}
+
+resource "null_resource" "example" {
+    triggers = {
+    value = "A example resource that does nothing!"
+    }
+}
